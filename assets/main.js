b0ba9f08273b6b4c5caaccd517c49522cdac3fe7
sethherr <seth.william.herr@gmail.com>
2022-01-25 08:23:44 -0800

    wip

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+// https://files.bikeindex.org/uploads/tsvs/stolen_geojson.json
+// L.mapbox.accessToken = "pk.eyJ1IjoiYmlrZWluZGV4IiwiYSI6ImNrZzc1OGUzYjAzeDUycW15MnJrcjJ3cjcifQ.ke32Jq5-9LuO2_7nONIK0w"
+
+console.log('party')
