b15647734efb68587781aa9f37b8f5b88f6eb05b
sethherr <seth.william.herr@gmail.com>
2022-01-25 08:54:35 -0800

    wip

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem "jekyll"
+gem "jekyll-environment-variables"
--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,16 @@
+---
+---
+
 // https://files.bikeindex.org/uploads/tsvs/stolen_geojson.json
-// L.mapbox.accessToken = "pk.eyJ1IjoiYmlrZWluZGV4IiwiYSI6ImNrZzc1OGUzYjAzeDUycW15MnJrcjJ3cjcifQ.ke32Jq5-9LuO2_7nONIK0w"
+
+var mapbox_key = '{{ site.env.MAPBOX_KEY }}'
+if (!mapbox_key.length) {
+  var mapbox_key =
+    'pk.eyJ1IjoiYmlrZWluZGV4IiwiYSI6ImNrZzc1OGUzYjAzeDUycW15MnJrcjJ3cjcifQ.ke32Jq5-9LuO2_7nONIK0w'
+}
 
 console.log('party')
+
+console.log(mapbox_key)
+
+console.log('c8zxcv')
